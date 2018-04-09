6a5fdeb6758eb1e81150112f74626b772806b0d1
yuanjianwen <35839725+yuanjianwen@users.noreply.github.com>
2018-04-10 00:59:53 +0800

    1

--- test.tcl
+++ test.tcl
@@ -2,17 +2,153 @@
 # test.tcl \
 exec tclsh "$0" ${1+"$@"}
 
-package require Tk  
+foreach {package version} {
+    Tk 8.4
+    mysqltcl 3.0
+    tile 0.7
+} {
+    if {[catch {package require $package $version} err]} {
+	error "couldn't load $package >= $version: $err"
+    }
+}
+proc showLoginWindow {} {
+    if {[winfo exists .login]} {
+	wm deiconify .login
+return
+    }
+    namespace eval ::ttk {
+	array set ::conn {
+	    username {GameMarket}
+	    password {w73194001}
+	    host 119.29.54.117
+	    port 3306
+	    database GameMarket
+	}
+	set w [toplevel .login -class LoginToplevel]
+	wm title $w "mysqltcl: login"
+	wm protocol $w WM_DELETE_WINDOW {destroy .}
 
-ttk::treeview .tree -columns "Creator Year" -displaycolumns "Year Creator" 
-.tree heading Creator -text "Creator" -anchor center
-.tree heading Year -text "Year" -anchor center
-pack .tree
-.tree insert {} end -id Languages -text "Languages"
-.tree insert Languages end -text C -values [list "Dennis Ritchie" "1990"]
-proc scaleMe {mywidget scaleValue} {
-	$mywidget configure -length $scaleValue
-} 
-pack [scale .s2  -from 100.0 -to 200.0 -length 100 -background yellow -borderwidth 5 -font {Helvetica -18 bold} -foreground red -width 40 -relief ridge -orien horizontal -variable a -command "scaleMe .s2" ]
-pack [ttk::progressbar .p1 -orient horizontal -length 200 -mode indeterminate -value 90]
-pack [ttk::progressbar .p2 -orient horizontal -length 200 -mode determinate -variable a -maximum 75 -value 20]
+	set f [frame $w.main]
+	label $f.userlbl -text "Username:"
+	entry $f.user -textvariable ::conn(username)
+	label $f.passlbl -text "Password:"
+	entry $f.pass -textvariable ::conn(password) -show "*"
+	label $f.hostlbl -text "Host:"
+	entry $f.host -textvariable ::conn(host)
+	label $f.portlbl -text "Port:"
+	entry $f.port -textvariable ::conn(port)
+	label $f.dblbl -text "Database:"
+	entry $f.db -textvariable ::conn(database)
+	button $f.connect -text "Connect" -command ::connect
+	foreach e {user pass host port db} {
+	    grid $f.${e}lbl $f.$e -sticky ew -pady {1m 0}
+	    grid $f.${e}lbl -sticky e
+	}
+	grid $f.connect - -sticky ew -pady {5m 0}
+	pack $f -expand true -fill both -padx 10m -pady 5m
+
+	set f [frame $w.status]
+	pack [label $f.text -relief sunken -borderwidth 2] -fill x -ipady 5
+	pack $f -fill x -pady {10 0}
+
+	bind .login <Return> {.login.main.connect invoke}
+	bind .login <Control-Shift-C> {console show}
+
+	# This dynamically resizes the status bar's width.
+	bind LoginToplevel <Configure> {
+	    .login.status.text configure \
+		-wraplength [expr {[winfo width %W] - 20}]
+	}
+    }
+}
+
+proc connect {} {
+    global conn
+    if {[catch {
+	set conn(handle) [::mysql::connect \
+	    -user $conn(username) -password $conn(password) \
+	    -host $conn(host) -port $conn(port) -db $conn(database)]
+    } err]} {
+	.login.status.text configure -text "could not connect: $err" \
+	    -background red
+    } else {
+	.login.status.text configure -text "Connected!" -background green
+	wm withdraw .login
+	showAppWindow
+    }
+}
+
+proc showAppWindow {} {
+    if {[winfo exists .app]} {
+	wm deiconify .app
+	return
+    }
+    namespace eval ::ttk {
+	set w [toplevel .app -class AppToplevel]
+	wm title $w "mysqltcl: $::conn(username)@$::conn(host):$::conn(port)"
+	wm protocol $w WM_DELETE_WINDOW {destroy .}
+
+	set f [frame $w.main]
+	pack $f -expand true -fill both -padx 10m -pady 5m
+	label $f.sqllbl -text "SQL:"
+	text $f.sql -width 40 -height 4 -font {Courier 10} \
+	    -yscrollcommand "$f.sqlvsb set"
+	scrollbar $f.sqlvsb -orient vertical -command "$f.sql yview"
+	button $f.go -text "Go" -command ::go
+	label $f.resultlbl -text "Result:"
+	text $f.result -width 40 -height 10 -font {Courier 10} \
+	    -yscrollcommand "$f.resultvsb set" \
+	    -state disabled -background "#e5e5e5"
+	scrollbar $f.resultvsb -orient vertical -command "$f.result yview"
+	set e sql
+	grid $f.${e}lbl $f.$e $f.${e}vsb -sticky nsew -pady {1m 0}
+	grid $f.${e}lbl -sticky ne
+	grid x $f.go -sticky ew -pady 10
+	set e result
+	grid $f.${e}lbl $f.$e $f.${e}vsb -sticky nsew -pady {1m 0}
+	grid $f.${e}lbl -sticky ne
+
+	grid columnconfigure $f 1 -weight 1
+	grid rowconfigure $f 2 -weight 1
+
+	bind .app.main.sql <Control-Return> {
+	    .app.main.go invoke
+	    .app.main.sql delete "end - 1 chars"
+	}
+	bind .app <Control-Shift-C> {console show}
+    }
+}
+
+proc go {} {
+    set f .app.main
+    $f.result configure -state normal
+    $f.result delete 0.0 end
+    $f.result insert end "Executing query ..."
+    $f.result configure -state disabled
+    update ;# to refresh UI
+    $f.result configure -state normal
+    $f.result delete 0.0 end
+    $f.result configure -state disabled
+    set db $::conn(handle)
+    set sql [$f.sql get 0.0 end]
+    if {[catch {set query [::mysql::query $db $sql]} err]} {
+	$f.result configure -state normal
+	$f.result insert end $err
+	$f.result configure -state disabled
+	return
+    }
+    while {[llength [set row [::mysql::fetch $query]]]} {
+	$f.result configure -state normal
+	$f.result insert end $row
+	$f.result insert end "\n"
+	$f.result configure -state disabled
+	update ;# to refresh UI
+    }
+    ::mysql::endquery $query
+}
+
+#
+# This starts the application, with the login dialog.
+#
+
+showLoginWindow
