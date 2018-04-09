2080ae829ef12c08dbd5529be05496b98fffd1d5
yuanjianwen <35839725+yuanjianwen@users.noreply.github.com>
2018-04-09 11:40:41 +0800

    1

--- MainPage.tcl
+++ MainPage.tcl
@@ -2,8 +2,8 @@
 # MainPage.tcl \
 
 package require Tk  
-package require mysqltcl 3.03
-package require tdbc
+
+
 #首先定义窗口尺寸,比例,透明度
 wm maxsize . 2160 1600
 wm minsize . 1024 768
@@ -16,3 +16,4 @@ set dbuser GameMarket
 set dbpassword "w73194001"
 set dbank GameMarket
 
+puts stdout [package ifneeded tcom 3.9]
