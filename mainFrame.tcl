14b77b35e7f7426c291778e8eb477ac184fb611a
yuanjianwen <35839725+yuanjianwen@users.noreply.github.com>
2018-04-12 04:09:51 +0800

    1

--- mainFrame.tcl
+++ mainFrame.tcl
@@ -31,19 +31,22 @@ proc showAppWindow {} {
 		wm maxsize $w 3840 2160
 		font create HeaderFont -family Helvetica -size 18 -weight bold 
 		#创建市场大标题frame
-		frame $w.frame_Market_Header  -borderwidth 8 -background black 
+		frame $w.frame_Market_Header  -borderwidth 8 -background black -borderwidth 2m
 		#创建主体内容frame
-		frame $w.frame_Main_Content -borderwidth 8 -background SlateBlue
+		frame $w.frame_Main_Content -borderwidth 8 -background SlateBlue -borderwidth 2m
 		#创建右侧游戏列表frame
-		frame $w.frame_Main_Content.frame_side_bar -borderwidth 8 -background DarkSlateBlue
+		frame $w.frame_Main_Content.frame_side_bar -borderwidth 8 -background DarkSlateBlue -borderwidth 2m
 		#创建左侧道具列表frame
-		frame $w.frame_Main_Content.frame_popular_list -borderwidth 8 -background DarkCyan
+		frame $w.frame_Main_Content.frame_popular_list -borderwidth 8 -background DarkCyan -borderwidth 2m
 		
 		#安装全套frame组件
 		pack $w.frame_Market_Header -fill both -side top
 		pack $w.frame_Main_Content -fill both -expand yes 
+		#左
+		pack $w.frame_Main_Content.frame_popular_list -expand yes -fill both -side left
+		#右
 		pack $w.frame_Main_Content.frame_side_bar -fill y  -side right 
-		pack $w.frame_Main_Content.frame_popular_list -fill both -expand yes -side left
+		
 		
 		#创建大标题和描述
 		label $w.frame_Market_Header.lable_market_head -text "社区市场   "  -font HeaderFont
@@ -52,9 +55,11 @@ proc showAppWindow {} {
 		pack $w.frame_Market_Header.lable_market_head -side left
 		pack $w.frame_Market_Header.lable_market_content -side left
 		
-		#创建安装右侧热门游戏listbox和标题
-		label $w.frame_Main_Content.frame_side_bar.label_popular_list -text 热门游戏列表
-		frame  $w.frame_Main_Content.frame_side_bar.frame_Game_list
+		#创建右侧热门游戏listbox和标题
+		label $w.frame_Main_Content.frame_side_bar.label_popular_list -text "热门游戏列表"
+		frame  $w.frame_Main_Content.frame_side_bar.frame_Game_list 
+		
+		#安装右侧热门游戏listbox和标题
 		pack $w.frame_Main_Content.frame_side_bar.label_popular_list
 		pack $w.frame_Main_Content.frame_side_bar.frame_Game_list -fill both -expand yes
 		
@@ -62,16 +67,45 @@ proc showAppWindow {} {
 		label $w.frame_Main_Content.frame_popular_list.lable_items -text 热门物品
 		frame $w.frame_Main_Content.frame_popular_list.frame_items 
 		
-		#安装左侧热门商品tree
+		#安装左侧热门商品frame
 		pack $w.frame_Main_Content.frame_popular_list.lable_items
 		pack $w.frame_Main_Content.frame_popular_list.frame_items -fill both -expand yes 
 		
-		proc add_popular_item {} {
+		
+		proc add_popular_item {itemName img_path} {
+			
+			variable  w
+			frame $w.frame_Main_Content.frame_popular_list.frame_items.frame_item -borderwidth 2m -bg red
+			pack $w.frame_Main_Content.frame_popular_list.frame_items.frame_item -fill x 
+			image create photo imgobj{$img_path} -file $img_path -width 120 -height 120
+			
+			label $w.frame_Main_Content.frame_popular_list.frame_items.frame_item.imgitem_{$itemName}
+			$w.frame_Main_Content.frame_popular_list.frame_items.frame_item.imgitem_{$itemName} configure -image imgobj{$img_path} -bg red
+			
+			label $w.frame_Main_Content.frame_popular_list.frame_items.frame_item.item_{$itemName} -text $itemName
+			pack $w.frame_Main_Content.frame_popular_list.frame_items.frame_item.imgitem_{$itemName} -side left 
+			pack $w.frame_Main_Content.frame_popular_list.frame_items.frame_item.item_{$itemName} -side left
 			
 		}
-		proc add_popular_game {} {
+		proc add_game {gameName img_path} {
+			
+			variable  w
+			frame $w.frame_Main_Content.frame_side_bar.frame_Game_list.frame_Game -borderwidth 2m -bg red
+			pack $w.frame_Main_Content.frame_side_bar.frame_Game_list.frame_Game -fill x 
+			
+			image create photo imgobj{$img_path} -file $img_path -width 40 -height 40
+			
+			label $w.frame_Main_Content.frame_side_bar.frame_Game_list.frame_Game.imgitem_{$gameName}
+			$w.frame_Main_Content.frame_side_bar.frame_Game_list.frame_Game.imgitem_{$gameName} configure -image imgobj{$img_path} -bg red
+			
+			label $w.frame_Main_Content.frame_side_bar.frame_Game_list.frame_Game.item_{$gameName} -text $gameName
+			pack $w.frame_Main_Content.frame_side_bar.frame_Game_list.frame_Game.imgitem_{$gameName} -side left 
+			pack $w.frame_Main_Content.frame_side_bar.frame_Game_list.frame_Game.item_{$gameName}  -side left 
+			
 					
 		}
+		add_popular_item Key "./item_img/key.png"
+		add_game csgo "./game_img/csgo.png"
 
 		
 	}
